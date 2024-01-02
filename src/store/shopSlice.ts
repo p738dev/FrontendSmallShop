72a1c4ad62b784accdcce6abc044d9af9c810bde
p738dev <pmech14@interia.pl>
2024-01-02 12:25:32 +0100

    SMF-13 Change in getshopproducts function

--- src/store/shopSlice.ts
+++ src/store/shopSlice.ts
@@ -23,7 +23,6 @@ export const getShopProducts = createAsyncThunk(
       const response = await axios.get(
         `http://www.api.piotrgruca.pl/api/products?page=${page}`
       );
-      console.log(response.data);
       return response.data;
     } catch (error) {}
   }
